639dec565ed41b727e64fb332d1b864b42c1aab6
Polina-leonova <polina.leonova665@mail.ru>
2026-07-19 19:36:59 +0300

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,34 +1,61 @@
 from django.contrib import admin
-from .models import User, Organization, Service, Order
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import (
+    User, Organization, OrganizationImage, Service, 
+    ServiceImage, Order, OrderItem, OrganizationSubmission
+)
 
-# Класс для отображения услуг внутри организации
-class ServiceInline(admin.TabularInline):
+class OrganizationImageInline(admin.TabularInline):
+    model = OrganizationImage
+    extra = 1
+
+class ServiceImageInline(admin.TabularInline):
+    model = ServiceImage
+    extra = 1
+
+class ServiceInline(admin.StackedInline):
     model = Service
-    extra = 1 # Сколько пустых строк для новых услуг показывать сразу
+    extra = 0
+    fields = (
+        'name', 'description', 'price', 'category', 
+        'capacity', 'amenities', 'date_start', 
+        'date_end', 'itinerary', 'included'
+    )
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ('is_redeemed',)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Упрощенный админ для пользователя во избежание конфликтов fieldsets"""
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_superuser', 'city')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email')
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('name', 'org_type', 'city', 'is_verified')
     list_filter = ('is_verified', 'org_type')
     list_editable = ('is_verified',)
-    inlines = [ServiceInline]
+    inlines = [OrganizationImageInline, ServiceInline]
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'organization', 'price')
+    list_display = ('name', 'organization', 'price', 'category')
+    list_filter = ('organization', 'category')
+    inlines = [ServiceImageInline]
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'promo_code', 'is_paid')
+    list_display = ('id', 'user', 'is_paid', 'promo_code', 'created_at')
+    list_filter = ('is_paid',)
+    inlines = [OrderItemInline]
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'role', 'city', 'is_staff')
-    
-    fieldsets = BaseUserAdmin.fieldsets + (
-        ('Дополнительная информация', {'fields': ('role', 'phone', 'age', 'gender', 'country', 'city')}),
-    )
-    add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        ('Дополнительная информация', {'fields': ('role', 'phone', 'age', 'gender', 'country', 'city')}),
-    )
+@admin.register(OrganizationSubmission)
+class OrganizationSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'org_type', 'submitted_by', 'status', 'created_at')
+    list_filter = ('status', 'org_type')
+    readonly_fields = ('created_at', 'reviewed_at')
+
+admin.site.register(OrderItem)
